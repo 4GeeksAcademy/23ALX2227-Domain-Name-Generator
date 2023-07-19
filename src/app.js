4a35f90ae9ea29b98edd5e100e6011c797b23d71
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-07-19 00:28:36 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let emptyArray = [];
+  for (let i in pronoun)
+    for (let b in adj)
+      for (let c in noun)
+        emptyArray.push(pronoun[i] + adj[b] + noun[c] + ".com <br>");
+  console.log(emptyArray);
+  document.querySelector(".domains").innerHTML = emptyArray;
 };
